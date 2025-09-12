b50f29f17818ed73473efc79823821d67abcd789
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2025-09-11 23:50:18 -0700

    add gsap animations on the herro section

--- src/components/content/herro-section.tsx
+++ src/components/content/herro-section.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import { ImageBackground } from "../ui/image-background";
 import { socialMedia } from "@/lib/docs";
 import {
@@ -8,37 +9,157 @@ import {
   TooltipTrigger,
 } from "../ui/tooltip";
 import DownloadCVButton from "../ui/download-cv-button";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(SplitText);
 
 const HeroSection = () => {
+  const name = useRef(null);
+  const subtitle1 = useRef(null);
+  const subtitle2 = useRef(null);
+  const downloadCVButton = useRef(null);
+  const imageBackground = useRef(null);
+
+  useGSAP(() => {
+    const split = SplitText.create(name.current, {
+      type: "chars, words, lines",
+      mask: "chars",
+    });
+    split.chars.forEach((char) => {
+      char.classList.add(
+        "bg-clip-text",
+        "bg-gradient-to-b",
+        "from-black",
+        "via-slate-500",
+        "to-white",
+        "text-transparent"
+      );
+    });
+    gsap.from(split.chars, {
+      y: 100,
+      opacity: 0,
+      scale: 0.8,
+      duration: 1,
+      autoAlpha: 0,
+      ease: "power2.inOut",
+      stagger: {
+        amount: 0.3,
+      },
+    });
+
+    gsap.from(subtitle1.current, {
+      x: -100,
+      scale: 0.8,
+      opacity: 0,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+
+    gsap.from(subtitle2.current, {
+      x: 100,
+      scale: 0.8,
+      opacity: 0,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+
+    gsap.from(downloadCVButton.current, {
+      y: 100,
+      scale: 0.8,
+      opacity: 0,
+      duration: 1,
+      delay: 1,
+      ease: "power2.inOut",
+    });
+
+    // Mobile animation (y: -50, x: 0)
+    gsap.matchMedia().add("(max-width: 768px)", () => {
+      gsap.from(".social-media-link", {
+        y: -50,
+        scale: 0.8,
+        opacity: 0,
+        delay: 1,
+        ease: "power2.inOut",
+        stagger: {
+          amount: 0.5,
+        },
+      });
+    });
+
+    // Desktop animation (x: -100, y: 0)
+    gsap.matchMedia().add("(min-width: 769px)", () => {
+      gsap.from(".social-media-link", {
+        x: -100,
+        scale: 0.8,
+        opacity: 0,
+        delay: 1,
+        ease: "power2.inOut",
+        stagger: {
+          amount: 0.5,
+        },
+      });
+    });
+
+    gsap.from(imageBackground.current, {
+      y: 200,
+      opacity: 0,
+      duration: 1,
+      delay: 1.2,
+      ease: "power2.inOut",
+    });
+  });
 
   return (
     <>
-      <section className="flex flex-col bg-gradient-to-b from-white to-[#D4EAF6] justify-center items-center h-[calc(100vh+120px)] w-screen p-5 md:p-10 xl:p-20">
-        <ImageBackground />
+      <section className="flex flex-col bg-gradient-to-b from-white to-[#D4EAF6] justify-center items-center h-[calc(100vh+120px)] w-screen 0.8 md:p-10 xl:p-20">
+        <div
+          ref={imageBackground}
+          className="absolute overflow-hidden bg-transparent z-20 w-screen max-h-[calc(100vh+120px)]"
+        >
+          <ImageBackground />
+        </div>
         <div className="relative w-full mt-16 md:mt-0 h-fit z-10 flex justify-center items-start translate-y-10">
           <div className="flex flex-col">
             <h3
-              className="bg-clip-text bg-gradient-to-r from-slate-600 to-gray-400
-              font-bebasNeue text-transparent text-center
+              className="
+              inline-flex gap-2 justify-center items-center
+              font-bebasNeue text-center
               text-2xl tracking-wider
               sm:text-2xl sm:tracking-[7.5px]
               lg:text-3xl
               lg:tracking-[16.5px]
               "
             >
-              Software Engineer | Full-stack dev
+              <div
+                className="bg-clip-text bg-gradient-to-b from-slate-600 to-gray-400 text-transparent"
+                ref={subtitle1}
+              >
+                Software Engineer
+              </div>
+              <span className="bg-clip-text bg-gradient-to-b from-slate-600 to-gray-400 text-transparent">
+                |
+              </span>
+              <div
+                className="bg-clip-text bg-gradient-to-b from-slate-600 to-gray-400 text-transparent"
+                ref={subtitle2}
+              >
+                Full-stack dev
+              </div>
             </h3>
             <h1
-              className="bg-clip-text bg-gradient-to-b from-black via-slate-500 to-white
-              font-bebasNeue font-black text-transparent text-center
-              text-7xl tracking-tighter 
-              sm:text-[145px] sm:tracking-[-7px]
-              lg:text-[230px] lg:tracking-[-5px]
+              ref={name}
+              className="
+              font-bebasNeue font-black text-center
+              tracking-tight
+              text-7xl 
+              leading-none
+              sm:text-[145px]
+              lg:text-[230px]
               "
             >
-              {/*
-              leading-[50px] sm:leading-[80px] lg:leading-[135px]
-                */}
               Sebastjan Bas
             </h1>
           </div>
@@ -47,35 +168,36 @@ const HeroSection = () => {
           <div className="relative max-w-2xl lg:max-w-5xl flex flex-col sm:flex-row w-full  h-fit justify-center sm:justify-between items-center gap-y-0 sm:items-start px-10">
             <div className="flex flex-row md:flex-col gap-5">
               {socialMedia.map((item) => (
-                <div
-                  key={item.id}
-                  className="inline-flex justify-start items-center gap-5 transition-transform duration-200 hover:scale-110"
-                >
-                  <a href={item.href}>
-                    <TooltipProvider delayDuration={300}>
-                      <Tooltip>
-                        <TooltipTrigger>
-                          <item.Component
-                            size="size-7"
-                            fillColor="fill-slate-600"
-                          />
-                        </TooltipTrigger>
-                        <TooltipContent side="right" className="bg-slate-500">
-                          {item.name}
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </a>
+                <div key={item.id} className="social-media-link">
+                  <div className="inline-flex justify-start items-center gap-5 transition-transform duration-200 hover:scale-110">
+                    <a href={item.href}>
+                      <TooltipProvider delayDuration={300}>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <item.Component
+                              size="size-7"
+                              fillColor="fill-slate-600"
+                            />
+                          </TooltipTrigger>
+                          <TooltipContent side="right" className="bg-slate-500">
+                            {item.name}
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </a>
+                  </div>
                 </div>
               ))}
             </div>
-            <div
-              className="text-slate-600 font-semibold font-bebasNeue text-lg text-center leading-none -translate-y-40 sm:translate-y-2 
+            <div ref={downloadCVButton}>
+              <div
+                className="text-slate-600 font-semibold font-bebasNeue text-lg text-center leading-none -translate-y-44 sm:translate-y-2 
               md:after:block md:after:h-[1px] md:after:origin-left md:after:scale-x-0 md:after:bg-slate-600 
               md:after:transition-transform md:after:duration-500 
               md:hover:after:scale-x-100 md:hover:tracking-wider transition-all will-change-transform"
-            >
-              <DownloadCVButton />
+              >
+                <DownloadCVButton />
+              </div>
             </div>
           </div>
         </div>
--- src/components/ui/image-background.tsx
+++ src/components/ui/image-background.tsx
@@ -15,10 +15,10 @@ export const ImageBackground = () => {
     const updateScrollRange = () => {
       if (window.innerWidth < 768) {
         setScrollRange([0, 0.55]); // Increase range for slower effect on mobile
-        setScreenWidth("mobile")
+        setScreenWidth("mobile");
       } else {
         setScrollRange([-0.35, 0.35]); // Default for Desktop
-        setScreenWidth("desktop")
+        setScreenWidth("desktop");
       }
     };
 
@@ -38,10 +38,10 @@ export const ImageBackground = () => {
   const strokeDashoffset = useTransform(
     scrollYProgress,
     [0, 0.15],
-    [strokeLength, 0],
-  )
+    [strokeLength, 0]
+  );
   return (
-    <div className="absolute overflow-hidden bg-transparent z-20 w-screen max-h-[calc(100vh+120px)]">
+    <>
       <motion.div
         className="relative overflow-x-visible flex justify-center items-center top-0 h-[calc(100vh+100px)] md:h-[calc(100vh+890px)] -translate-y-10 md:-translate-y-20 w-full bg-transparent"
         style={{
@@ -70,7 +70,7 @@ export const ImageBackground = () => {
           <motion.path
             d="M669.001 420C669.001 420 1168 183.5 952.001 240.001C736.002 296.501 440.831 509.659 489.001 600C537.17 690.342 1048.96 360.026 1072.5 441.5C1096.04 522.975 -424.926 1412.31 125.074 982.313C675.074 552.312 2231.07 340.001 2231.07 359.001C2231.07 359.001 1008.84 594.857 1002 596.5C995.165 598.144 1190 422.5 1135.5 432C1081 441.5 1072.5 531 1072.5 531C1144.23 598.593 1269.5 463.001 1231.5 250.501C1193.5 38.0006 1213.83 606.995 1231.5 508C1236.75 478.555 1213.71 444.259 1241 432C1308.41 401.714 1344.12 497.285 1333.5 432C1322.88 366.715 1344.75 532.338 1260.5 494.5C1176.25 456.663 1482 370.501 1482 370.501C1482 370.501 1423 388 1396.5 404C1370 420 1519 398.5 1528.5 420C1538 441.5 1338.88 525.361 1387 487.5C1702.48 239.255 1608.5 -178.501 1586.5 95.9998C1564.5 370.501 1449.5 567.5 1500.5 508C1551.5 448.501 1610 420 1639 305.501C1668 191.001 1586.5 661.5 1427.5 796.001C1268.5 930.502 1698.5 223.437 1694.5 340.001C1690.5 456.565 1814.86 356 1679.5 432C1544.14 508 1774.5 324.001 1774.5 324.001C1774.5 324.001 1763 240.784 1763 370.501C1763 500.217 1872.69 253.813 1977.5 305.501C2082.3 357.189 1783 156.501 1260.5 263.501C737.998 370.501 2492 213.001 2556 145.001C2620 77 1354 477.001 1679.5 542.5C2005 608 2504 118.5 2525 221.001C2546 323.501 2549.5 414.5 2515.5 441.5C2481.5 468.501 2296.5 303.501 2304.5 278.001C2312.5 252.5 2761 136.501 2722.5 165.5C2684 194.499 2264.24 417.001 2326 370.501C2387.76 324.001 2617.5 267 2722.5 263.501C2827.5 260.001 2185 522.001 2086 542.5C1987 563 2798.5 221.001 2760.5 278.001C2722.5 335.001 2683.5 402 2641.5 380.501C2599.5 359.001 2777.5 253.266 2760.5 305.501C2752.57 329.869 2882 292.001 2722.5 359.001C2563 426 2874 282.501 2965 263.501C3056 244.501 2914.5 402.001 2618.5 486.501"
             stroke="black"
-            strokeWidth={screenWidth === "mobile" ? "10": "8"}
+            strokeWidth={screenWidth === "mobile" ? "10" : "8"}
             className={"stroke-foreground"}
             strokeDasharray={strokeLength}
             style={{ strokeDashoffset }}
@@ -79,6 +79,6 @@ export const ImageBackground = () => {
           />
         </svg>
       </motion.div>
-    </div>
+    </>
   );
 };
